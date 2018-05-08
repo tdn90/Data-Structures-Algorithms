7e31947e7bd91068c8d9641eeb14598328089ba0
tdn90 <nguyentridung1212@gmail.com>
2018-05-07 23:48:25 -0400

    Finished implementing Counting Sort

--- src/algorithms/SortingAlgs/Counting.java
+++ src/algorithms/SortingAlgs/Counting.java
@@ -1,4 +1,11 @@
 package algorithms.SortingAlgs;
 
 public class Counting {
+    public static void sort(Integer[] values, int k) {
+        int[] copyRangeArr = new int[k + 1]; // O(k) space
+        for (Integer val : values) copyRangeArr[val]++; // n times
+        for (int i = 0, idx = 0; i < copyRangeArr.length; i++) {
+            for (int j = copyRangeArr[i]; j > 0; j--) values[idx++] = i;  // n + k times
+        }
+    }
 }
