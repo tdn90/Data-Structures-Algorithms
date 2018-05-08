d87f90367da2c51247d1781f34852fb3d2ddbe98
tdn90 <nguyentridung1212@gmail.com>
2018-05-07 23:47:14 -0400

    Finished implementing Selection Sort

--- src/algorithms/SortingAlgs/Selection.java
+++ src/algorithms/SortingAlgs/Selection.java
@@ -1,4 +1,13 @@
 package algorithms.SortingAlgs;
 
-public class Selection {
+public class Selection extends SortAlgs {
+    public static void sort(Comparable[] arr) {
+        for (int i = 0; i < arr.length-1; i++) {
+            int minIndex = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (less(arr,j,i)) minIndex = j;
+            }
+            swap(arr,i,minIndex);
+        }
+    }
 }
