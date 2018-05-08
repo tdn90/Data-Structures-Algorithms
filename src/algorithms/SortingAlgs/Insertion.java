5480a27e839368e45be7619254b3f1f946c83559
tdn90 <nguyentridung1212@gmail.com>
2018-05-07 23:47:34 -0400

    Finished implementing Insertion Sort

--- src/algorithms/SortingAlgs/Insertion.java
+++ src/algorithms/SortingAlgs/Insertion.java
@@ -1,4 +1,17 @@
 package algorithms.SortingAlgs;
 
-public class Insertion {
+import java.util.Arrays;
+
+public class Insertion{
+    public static void sort(Comparable[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            Comparable cur = arr[i];
+            while (j >= 1 && cur.compareTo(arr[j-1]) < 0) {
+                arr[j] = arr[j-1];
+                j--;
+            }
+            arr[j] = cur;
+        }
+    }
 }
