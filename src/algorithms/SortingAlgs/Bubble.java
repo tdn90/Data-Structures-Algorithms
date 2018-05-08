4200ed3bc6139f9ca131a2b539112020f4256239
tdn90 <nguyentridung1212@gmail.com>
2018-05-07 23:46:58 -0400

    Finished implementing Bubble Sort

--- src/algorithms/SortingAlgs/Bubble.java
+++ src/algorithms/SortingAlgs/Bubble.java
@@ -1,4 +1,16 @@
 package algorithms.SortingAlgs;
 
-public class Bubble {
+public class Bubble extends SortAlgs{
+    public static void sort(Comparable[] arr) {
+        boolean sorted = false;
+        while (sorted!=true) {
+            sorted = true;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (less(arr,i+1,i)) {
+                    swap(arr, i, i + 1);
+                    sorted = false;
+                }
+            }
+        }
+    }
 }
