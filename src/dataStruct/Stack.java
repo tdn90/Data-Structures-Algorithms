b9512172ba7d0fc669505a2013b146c4fb7b04ee
tdn90 <nguyentridung1212@gmail.com>
2018-05-07 23:42:56 -0400

    Add some specs to Stack Interface

--- src/dataStruct/Stack.java
+++ src/dataStruct/Stack.java
@@ -13,19 +13,21 @@ public interface Stack<T> {
     /**
      * Looks at top of the Stack without removing it.
      * @return the top of this Stack; return null if Stack is empty
+     * @throws java.util.EmptyStackException if the stack is empty
      */
     T peek();
 
     /**
      * Retrieves and removes the object on top of this Stack
      * @return the object on the top of this Stack upon its removal
+     * @throws java.util.EmptyStackException if the stack is empty
      */
     T pop();
 
     /**
      * Pushes an element on top of this Stack
      * @param obj: object to be pushed on stack
-     * @return the object itself
+     * @return true if an object can be pushed into stack; false otherwise
      */
-    T push(T obj);
+    boolean push(T obj);
 }
