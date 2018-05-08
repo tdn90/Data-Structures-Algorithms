071d35a9ba202ec97d733ae2b31d6309b90baa0d
tdn90 <nguyentridung1212@gmail.com>
2018-05-07 23:48:04 -0400

    Finished implementing Merge Sort in two different ways

--- src/algorithms/SortingAlgs/Merge.java
+++ src/algorithms/SortingAlgs/Merge.java
@@ -1,4 +1,54 @@
 package algorithms.SortingAlgs;
 
-public class Merge {
+import java.util.Arrays;
+
+public class Merge extends SortAlgs{
+    private static Comparable[] extra;
+
+    public static void sort(Comparable[] arr) {
+        int size = arr.length;
+        extra = new Comparable[size];
+        for (int i = 0; i < size; i++) extra[i] = arr[i];
+        sortHelper(arr,0,size-1);
+    }
+
+    public static void sortHelper(Comparable[] arr, int low, int high){
+        if (low >= high) return;
+        int mid = (low+high)/2;
+        sortHelper(arr, low, mid);
+        sortHelper(arr, mid+1, high);
+        merge(arr, low, mid, high);
+    }
+
+    public static void merge(Comparable[] arr, int low, int mid, int high) {
+        for (int i = low; i <= high; i++) extra[i] = arr[i];
+        int i = low;
+        int j = mid+1;
+        int k = low;
+
+        while (k <= high) {
+            if (i > mid) { // run out of items in the left-half array
+                arr[k++] = extra[j++];
+            }
+            else if (j > high) { // run out of items in the right-half array
+                arr[k++] = extra[i++];
+            }
+            else if (less(extra,i,j)) { // current item in the left array is smaller than the current value in right array
+                arr[k++] = extra[i++];
+            }
+            else { // current item in the right array is smaller than the current value in left array
+                arr[k++] = extra[j++];
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Integer[] arr = new Integer[8];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random()*100);
+        }
+        System.out.println(Arrays.deepToString(arr));
+        sort(arr);
+        System.out.println(Arrays.deepToString(arr));
+    }
 }
---
+++ src/algorithms/SortingAlgs/Merge2.java
@@ -0,0 +1,79 @@
+package algorithms.SortingAlgs;
+
+import java.util.Arrays;
+
+/**
+ * Second version of Merge Sort
+ */
+public class Merge2 {
+    /**
+     * Implementation of MergeSort.
+     * @param arr: given array to be sorted
+     * @return a sorted array in ascending order
+     * @note: Here, as I am self-implementing this mergeSort,
+     * I realize I requested some memory storage to create new smaller arrays.
+     * which is one draw back of improving time complexity.
+     */
+    public static Comparable[] sort(Comparable[] arr) {
+
+        // Base case: either empty array or array of length 1, return it.
+        if (arr.length < 2) return arr;
+
+        else {
+            int mid = arr.length / 2;
+
+            /*
+             * Divide the array into two halves
+             * Conquer each of them
+             */
+            Comparable[] firstHalf = sort(Arrays.copyOfRange(arr, 0, mid));
+
+            /*
+             * I made two mistakes here on my first try:
+             *
+             * As the end index of this function is exclusive, I should make it arr.length
+             *
+             * I put (mid + 1) as starting index at first
+             * However, it should be mid, as the firstHalf does not include the mid element
+             */
+            Comparable[] secondHalf = sort(Arrays.copyOfRange(arr, mid, arr.length));
+
+            // Finally, combine them together
+            return merge(firstHalf, secondHalf);
+        }
+    }
+
+    /**
+     * Helper method for mergeSort.
+     * Merge two array together so that all elements maintains the ascending order
+     * @param arr1
+     * @param arr2
+     * @return the combined, sorted array
+     */
+    public static Comparable[] merge(Comparable[] arr1, Comparable[] arr2){
+        int size1 = arr1.length;
+        int size2 = arr2.length;
+
+        Comparable[] result = new Comparable[size1 + size2];
+
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        /*
+         * Here, try to fill up the resulting array as much as possible
+         */
+        while (i < size1 && j < size2) {
+            if (arr1[i].compareTo(arr2[j]) < 0) result[k++] = arr1[i++];
+            else result[k++] = arr2[j++];
+        }
+
+        // Fill up the result array with whatever is left of arr1.
+        while (i < arr1.length) result[k++] = arr1[i++];
+
+        // Fill up the result array with whatever is left of arr2.
+        while (j < arr2.length) result[k++] = arr2[j++];
+
+        return result;
+    }
+}
