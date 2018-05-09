6a61877c286be860d20f176ab47e98e958d404f7
tdn90 <nguyentridung1212@gmail.com>
2018-05-09 01:05:24 -0400

    Finished implementing iterator() for ArrayList

--- src/dataStruct/ArrayList.java
+++ src/dataStruct/ArrayList.java
@@ -1,6 +1,7 @@
 package dataStruct;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * This is my self-implemented version of Java's / Oracle's ArrayList
@@ -135,8 +136,40 @@ public class ArrayList<T> implements List<T> {
      */
     @Override
     public Iterator<T> iterator() {
-        //TODO: implement this
-        return null;
+        return new ArrayListIterator<>();
+    }
+
+    private class ArrayListIterator<T> implements Iterator<T>{
+        private int curIdx;
+
+        public ArrayListIterator() {
+            curIdx = 0;
+        }
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return curIdx < lastEmptyIndex;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public T next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            else {
+                return (T) arr[curIdx++];
+            }
+        }
     }
 
     /**
