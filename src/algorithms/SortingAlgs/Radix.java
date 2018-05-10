e625dfe732ca81b790f43c1632252de67a3d4dd8
tdn90 <nguyentridung1212@gmail.com>
2018-05-09 22:57:47 -0400

    Complete implementation of Radix Sort (my version)

--- src/algorithms/SortingAlgs/Radix.java
+++ src/algorithms/SortingAlgs/Radix.java
@@ -3,8 +3,6 @@ package algorithms.SortingAlgs;
 import dataStruct.LinkedList;
 import dataStruct.Queue;
 
-import java.util.Arrays;
-
 public class Radix {
     public static void sort(int[] arr) {
         int max = findMax(arr);
@@ -12,7 +10,7 @@ public class Radix {
         for (int i = 0; i < maxDigit; i++) countingSort(arr, i);
     }
 
-    public static void countingSort(int[] arr, int numDigit) {
+    private static void countingSort(int[] arr, int numDigit) {
         Queue[] baskets = new Queue[10];
         // Initiates all baskets
         for (int m = 0; m < baskets.length; m++) {
@@ -20,9 +18,9 @@ public class Radix {
         }
 
         // Place each number in their according "basket" based on their current numDigit
-        for (int i = 0; i < arr.length; i++) {
-            int curDigit = getNumDigit(arr[i], numDigit);
-            baskets[curDigit].enqueue(arr[i]);
+        for (int anArr : arr) {
+            int curDigit = getNumDigit(anArr, numDigit);
+            baskets[curDigit].enqueue(anArr);
         }
 
         // Copy those numbers from baskets back to original array
@@ -38,7 +36,7 @@ public class Radix {
      * @param numDigit: specified num Digit to retrieve. NumDigit goes from left to right, starts from 0
      * @return the digit number at the specified digit location
      */
-    public static int getNumDigit(int num, int numDigit) {
+    private static int getNumDigit(int num, int numDigit) {
         return (int) (num / (Math.pow(10,numDigit))) % 10;
     }
 
@@ -47,7 +45,7 @@ public class Radix {
      * Pre-condition: arr.length > 0
      * @return the maximum integer in the array
      */
-    public static int findMax(int[] arr) {
+    private static int findMax(int[] arr) {
         int max = arr[0];
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) max = arr[i];
@@ -55,7 +53,7 @@ public class Radix {
         return max;
     }
 
-    public static int numDigits(int num) {
+    private static int numDigits(int num) {
         if (num == 0) return 1;
         int numDigit = 0;
         while (num > 0) {
@@ -63,24 +61,5 @@ public class Radix {
             numDigit++;
         }
         return numDigit;
-    }
-
-    public static boolean correct(int[] arr) {
-        for (int i = 0; i < arr.length-1; i++) {
-            if (arr[i] > arr[i+1]) return false;
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = new int[32];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (int) (Math.random()*10000);
-        }
-        System.out.println(Arrays.toString(arr));
-        System.out.println(arr.length);
-        sort(arr);
-        System.out.println(Arrays.toString(arr));
-        System.out.println(arr.length);
     }
 }
