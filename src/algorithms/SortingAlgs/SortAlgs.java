c505a5114d866a3c2cbc128266aa2bac77e9529f
tdn90 <nguyentridung1212@gmail.com>
2018-05-09 22:53:28 -0400

    MOdify parent Sorting class

--- src/algorithms/SortingAlgs/SortAlgs.java
+++ src/algorithms/SortingAlgs/SortAlgs.java
@@ -1,13 +1,13 @@
 package algorithms.SortingAlgs;
 
-public class SortAlgs {
-    public static void swap(Comparable[] arr, int i, int j) {
+class SortAlgs {
+    static void swap(Comparable[] arr, int i, int j) {
         Comparable cur = arr[i];
         arr[i] = arr[j];
         arr[j] = cur;
     }
 
-    public static boolean less(Comparable[] arr, int i, int j) {
+    static boolean less(Comparable[] arr, int i, int j) {
         return arr[i].compareTo(arr[j]) < 0;
     }
 }
