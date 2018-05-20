797100620869852b23797d80d44c017a64a82fe3
tdn90 <nguyentridung1212@gmail.com>
2018-05-20 19:31:25 -0400

    Complete documentation of Sort Algs

--- src/algorithms/SortingAlgs/SortAlgs.java
+++ src/algorithms/SortingAlgs/SortAlgs.java
@@ -1,12 +1,22 @@
 package algorithms.SortingAlgs;
 
+/**
+ * @author nguye
+ * This is a parent class that provides some common utils for sorting
+ */
 class SortAlgs {
+    /**
+     * Exchange elements between two given indices in the given array
+     */
     static void swap(Comparable[] arr, int i, int j) {
         Comparable cur = arr[i];
         arr[i] = arr[j];
         arr[j] = cur;
     }
 
+    /**
+     * Compare between two items
+     */
     static boolean less(Comparable[] arr, int i, int j) {
         return arr[i].compareTo(arr[j]) < 0;
     }
