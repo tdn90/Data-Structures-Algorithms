de5c1a542489760d864007a1d8b573d22e0ec522
tdn90 <nguyentridung1212@gmail.com>
2018-05-20 20:19:34 -0400

    Complete documentation of Selection Sort

--- src/algorithms/SortingAlgs/Selection.java
+++ src/algorithms/SortingAlgs/Selection.java
@@ -1,5 +1,10 @@
 package algorithms.SortingAlgs;
 
+/**
+ * @author nguye
+ * The most common implementation of selection sort.
+ * Time-complexity: O(n^2)
+ */
 public class Selection extends SortAlgs {
     public static void sort(Comparable[] arr) {
         for (int i = 0; i < arr.length-1; i++) {
